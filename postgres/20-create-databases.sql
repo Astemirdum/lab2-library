1fe0d0caa7f374bedc1338beb6d3fbbc62209c5d
Asterio <dumanishev.a@yandex.ru>
2023-10-08 23:12:14 +0300

    ci cd

---
+++ postgres/20-create-databases.sql
@@ -0,0 +1,9 @@
+-- file: 20-create-database.sql
+CREATE DATABASE reservations;
+GRANT ALL PRIVILEGES ON DATABASE reservations TO program;
+
+CREATE DATABASE libraries;
+GRANT ALL PRIVILEGES ON DATABASE libraries TO program;
+
+CREATE DATABASE ratings;
+GRANT ALL PRIVILEGES ON DATABASE ratings TO program;
--- postgres/30-create-databases.sh
+++ postgres/30-create-databases.sh
@@ -4,4 +4,4 @@ set -e
 export VARIANT=4
 export SCRIPT_PATH=/docker-entrypoint-initdb.d/
 export PGPASSWORD=postgres
-psql -f "$SCRIPT_PATH/scripts/db-$VARIANT.sql"
+#psql -f "$SCRIPT_PATH/scripts/db-$VARIANT.sql"
